87f3bf3c5661bc366595b9925c980f1460478269
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-03-06 13:17:07 +0530

    Update URLs for models

--- server.sh
+++ server.sh
@@ -4,15 +4,16 @@
 mkdir -p models
 
 # The big generic model
-# wget -N https://translate.wmcloud.org/static/nllb200/nllb200-600M/config.json -P models/nllb200-600M
-# wget -N https://translate.wmcloud.org/static/nllb200/nllb200-600M/model.bin  -P models/nllb200-600M
-# wget -N https://translate.wmcloud.org/static/nllb200/nllb200-600M/sentencepiece.bpe.model -P models/nllb200-600M
-# wget -N https://translate.wmcloud.org/static/nllb200/nllb200-600M/shared_vocabulary.txt -P models/nllb200-600M
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb200-600M/config.json -P models/nllb200-600M
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb200-600M/model.bin -P models/nllb200-600M
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb200-600M/sentencepiece.bpe.model -P models/nllb200-600M
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb200-600M/shared_vocabulary.txt -P models/nllb200-600M
 
 
-wget -N https://translate.wmcloud.org/static/nllb200/nllb-wikipedia/config.json -P models/nllb-wikipedia
-wget -N https://translate.wmcloud.org/static/nllb200/nllb-wikipedia/model.bin  -P models/nllb-wikipedia
-wget -N https://translate.wmcloud.org/static/nllb200/nllb-wikipedia/sentencepiece.bpe.model -P models/nllb-wikipedia
-wget -N https://translate.wmcloud.org/static/nllb200/nllb-wikipedia/shared_vocabulary.txt -P models/nllb-wikipedia
+# Wikipedia optimized model with limited languages
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb-wikipedia/config.json -P models/nllb-wikipedia
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb-wikipedia/model.bin s-P models/nllb-wikipedia
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb-wikipedia/sentencepiece.bpe.model -P models/nllb-wikipedia
+wget -N https://people.wikimedia.org/~santhosh/nllb/nllb-wikipedia/shared_vocabulary.txt -P models/nllb-wikipedia
 
 gunicorn
