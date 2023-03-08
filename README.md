48b5b08386ef6a391007f26d70a303c10490d480
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-03-08 12:24:08 +0530

    Add statsd metrics reporting and ecs structured logging
    Bug: T331505
    Change-Id: I5d229c47b2cf64831720def950f6effcbf186300

--- README.md
+++ README.md
@@ -6,3 +6,25 @@ docker run -dp 3000:80 wikipedia-mt:latest
 ```
 
 Open http://0.0.0.0:3000/ using browser
+
+
+## Monitoring
+
+Application can be moniotred using graphite.
+Dun the graphite-statsd docker, and point the statsd-host to it
+
+```bash
+docker run -d\
+ --name graphite\
+ --restart=always\
+ -p 80:80\
+ -p 2003-2004:2003-2004\
+ -p 2023-2024:2023-2024\
+ -p 8125:8125/udp\
+ -p 8126:8126\
+ graphiteapp/graphite-statsd
+
+```
+
+Now change the `statsd_host` in gunicorn.conf.py or use --statsd-host commandline option of gunicorn.
+
--- translate.py
+++ translate.py
@@ -9,22 +9,42 @@ import yaml
 import multiprocessing
 import os
 import time
+import statsd
+import logging
+logging.config.fileConfig("logging.conf")
+
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-app = Flask(
-    __name__
-)
-model=None
-tokenizer=None
+statsd_host = "statsd.eqiad.wmnet"
+try:
+    statsd_client = statsd.StatsClient(statsd_host)
+except Exception:
+    logging.error(f"Could not connect to statsd host {statsd_host}")
+
+APP_NAME = "MachineTranslation"
+
+app = Flask(__name__)
+
+
+model = None
+tokenizer = None
 config = json.loads(Path("config.json").read_text())
 
-def get_languages()->dict:
+
+def get_languages() -> dict:
     return OrderedDict(sorted(config.get("languages").items(), key=lambda x: x[1]))
+
 
 @app.route("/", defaults={"path": ""})
 def index(path):
     return render_template("index.html", languages=get_languages())
+
+
+@app.before_request
+def before_request():
+    request.start_time = time.time()
+
 
 @app.after_request
 def after_request(response):
@@ -33,60 +53,70 @@ def after_request(response):
     response.headers.add("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
     return response
 
+
 @app.route("/api/spec")
 def show_spec():
-    with open('spec.yaml', 'r') as file:
+    with open("spec.yaml", "r") as file:
         spec = yaml.safe_load(file)
     return jsonify(spec)
+
 
 @app.route("/api/languages")
 def list_languages():
     return get_languages()
 
+
 @app.route("/healthz")
 def health():
-    translation = translate('eng_Latn', 'ibo_Latn', ['health'])
-    return '' if len(translation) and len(translation[0]) else False
+    translation = translate("eng_Latn", "ibo_Latn", ["health"])
+    return "" if len(translation) and len(translation[0]) else False
+
 
 @app.route("/api/translate/<source_lang>/<target_lang>", methods=["POST"])
 def translate_handler(source_lang, target_lang):
     text = request.json.get("text")
-    supported_languages=get_languages()
+    supported_languages = get_languages()
 
     if not source_lang in supported_languages:
         abort(400, description="Invalid source language.")
     if not target_lang in supported_languages:
         abort(400, description="Invalid target language.")
 
-    if len(text)> 10000:
-        abort(413, description="Request too large to handle. Maximum 10000 characters are supported.")
+    if len(text) > 10000:
+        abort(
+            413,
+            description="Request too large to handle. Maximum 10000 characters are supported.",
+        )
     sentences = text.strip().splitlines()
-    if len(sentences)> 25:
-        abort(413, description="Request too large to handle. Maximum 25 sentences are supported.")
+    if len(sentences) > 25:
+        abort(
+            413,
+            description="Request too large to handle. Maximum 25 sentences are supported.",
+        )
     start = time.time()
     tgt_text_lines = translate(source_lang, target_lang, sentences)
     end = time.time()
-    return jsonify(
-        translation='\n'.join(tgt_text_lines),
-        translationtime = end-start
-    )
+    translationtime=end - start
+    statsd_client.incr(f'{APP_NAME.lower()}.mt.{source_lang}.{target_lang}')
+    statsd_client.timing(f'{APP_NAME.lower()}.mt.timing', translationtime)
+    return jsonify(translation="\n".join(tgt_text_lines),translationtime=translationtime)
 
-def translate(src_lang, tgt_lang, sentences)->List[str]:
+
+def translate(src_lang, tgt_lang, sentences) -> List[str]:
     """
     Translate the text from source lang to target lang
     """
     global model, tokenizer
-    sentences_tokenized=[]
-    translation=[]
-    target_prefix=[]
+    sentences_tokenized = []
+    translation = []
+    target_prefix = []
     if not model:
         init()
     for sentence in sentences:
         sentences_tokenized.append(
-                tokenizer.encode(sentence, out_type=str) + ['</s>', src_lang]
+            tokenizer.encode(sentence, out_type=str) + ["</s>", src_lang]
         )
         target_prefix.append([tgt_lang])
-
 
     results = model.translate_iterable(
         sentences_tokenized,
@@ -100,9 +130,10 @@ def translate(src_lang, tgt_lang, sentences)->List[str]:
         translation.append(tokenizer.decode(result.hypotheses[0][1:]))
     return translation
 
-def getModel()->Translator:
+
+def getModel() -> Translator:
     return Translator(
-        config.get("model"), # Model
+        config.get("model"),  # Model
         # maximum number of batches executed in parallel.
         # => Increase this value to increase the throughput.
         inter_threads=multiprocessing.cpu_count(),
@@ -110,19 +141,22 @@ def getModel()->Translator:
         # => Increase this value to decrease the latency on CPU.
         intra_threads=multiprocessing.cpu_count(),
         device="auto",
-        compute_type='auto'
+        compute_type="auto",
     )
 
-def getTokenizer() ->SentencePieceProcessor:
+
+def getTokenizer() -> SentencePieceProcessor:
     sp = SentencePieceProcessor()
     sp.load(config.get("tokenizer"))
     return sp
+
 
 def init():
     global model, tokenizer
     model = getModel()
     tokenizer = getTokenizer()
-    print("Translator initialized")
+    logging.info("Translator initialized")
+
 
 if __name__ == "__main__":
     init()
