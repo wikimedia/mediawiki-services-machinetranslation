0364dbfb538a464a472aec4a46bdc07cc63c7481
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-04-05 10:57:20 +0530

    Automatic language detection for source content
    Uses pycld2 library
    
    Bug: T334465
    Change-Id: I331eff913dcf73e4b3132acaf0cf7c78c4383092

--- static/main.js
+++ static/main.js
@@ -1,10 +1,27 @@
-let allLanguages={}
+let allLanguages = {}
+
+function getSupportedLanguages() {
+    return fetch(`/api/languages`).then(response => response.json())
+}
+
+function detectLanguage(text) {
+    return fetch(`/api/detectlang`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text
+        })
+    }).then(response => response.json()).then(result => result.language)
+}
+
 function doTranslate() {
     document.getElementById('progress').style.display = "block";
-    const from= document.getElementById('source_lang').value;
-    const to= document.getElementById('target_lang').value;
-    const text= document.getElementById('source_content').value;
-    document.getElementById('status').innerText ='';
+    const from = document.getElementById('source_lang').value;
+    const to = document.getElementById('target_lang').value;
+    const text = document.getElementById('source_content').value;
+    document.getElementById('status').innerText = '';
     fetch(`/api/translate/${from}/${to}`, {
         method: 'POST',
         headers: {
@@ -21,52 +38,56 @@ function doTranslate() {
         })
 }
 
-function listSupportedTargetLanguages(sourceLang, allPairs){
+function listSupportedTargetLanguages(sourceLang, allPairs) {
     const tgt_selector = document.getElementById('target_lang');
     tgt_selector.innerHTML = '';
-    const targetLangs=allPairs[sourceLang];
-    for( lang in targetLangs){
+    const targetLangs = allPairs[sourceLang];
+    for (lang in targetLangs) {
         const nameGenerator = new Intl.DisplayNames('en', { type: 'language' });
         const displayName = nameGenerator.of(lang);
         const el = document.createElement("option");
         el.textContent = displayName;
         el.value = lang;
-        if (lang=='es'){
-            el.selected=true;
+        if (lang == 'es') {
+            el.selected = true;
         }
         tgt_selector.appendChild(el);
     }
-    tgt_selector.addEventListener("change", function(){
+    tgt_selector.addEventListener("change", function () {
         document.getElementById('target_content').innerText = ""
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    src_selector = document.getElementById('source_lang');
+    const src_selector = document.getElementById('source_lang');
+    const sourceElement = document.getElementById('source_content');
 
-    fetch(`/api/languages`)
-        .then(response => response.json())
-        .then(languages => {
-            allLanguages=languages
-            src_selector.innerHTML = '';
+    getSupportedLanguages().then(languages => {
+        allLanguages = languages
+        src_selector.innerHTML = '';
 
-            for( lang in languages){
-                const nameGenerator = new Intl.DisplayNames('en', { type: 'language' });
-                const displayName = nameGenerator.of(lang);
-                const el = document.createElement("option");
-                el.textContent = displayName;
-                el.value = lang;
-                if (lang=='en'){
-                    el.selected=true;
-                }
-                src_selector.appendChild(el);
+        for (lang in languages) {
+            const nameGenerator = new Intl.DisplayNames('en', { type: 'language' });
+            const displayName = nameGenerator.of(lang);
+            const el = document.createElement("option");
+            el.textContent = displayName;
+            el.value = lang;
+            if (lang == 'en') {
+                el.selected = true;
             }
-            listSupportedTargetLanguages('en',allLanguages )
+            src_selector.appendChild(el);
+        }
+        listSupportedTargetLanguages('en', allLanguages)
+    })
+
+    src_selector.addEventListener("change", () => {
+        const from = document.getElementById('source_lang').value;
+        listSupportedTargetLanguages(from, allLanguages)
+    });
+
+    sourceElement.addEventListener("input", () => {
+        detectLanguage(sourceElement.value).then(language => {
+            src_selector.value = language;
         })
-
-        src_selector.addEventListener("change", function(){
-            const from= document.getElementById('source_lang').value;
-            listSupportedTargetLanguages(from,allLanguages )
-        });
-
+    });
 });
