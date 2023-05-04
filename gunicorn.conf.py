8c0cc46ef77bfe436cdb42eff422b5902b68cf06
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-05-04 10:54:24 +0530

    Prevent memory leaks from workers
    Restart the worker after a given number of requests to avoid memory
    leaks if any.
    
    Change-Id: If1d54c95bafd5d924e62d38dbfd0ac18c97dd080

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -10,3 +10,10 @@ wsgi_app  = "translate:app"
 statsd_prefix = os.getenv('STATSD_PREFIX', "machinetranslation")
 statsd_host = f"{os.getenv('STATSD_HOST', 'localhost')}:{os.getenv('STATSD_PORT', 8125)}"
 logconfig = "logging.conf"
+
+# The maximum number of requests a worker will process before restarting.
+# This is a simple method to help limit the damage of memory leaks.if any
+max_requests = 1000
+# The maximum jitter to add to the max_requests setting.
+# This is intended to stagger worker restarts to avoid all workers restarting at the same time.
+max_requests_jitter = 50
