d557d5a19d272fad4a5532d32670ac54b3bb8553
Kartik Mistry <kartik.mistry@gmail.com>
2025-06-23 20:12:26 +0530

    Revert "entrypoint: Update model storage download"
    This reverts commit bfe690ff0b8bf0ada68ee8d0bbae890eca65e708.
    
    Also, force wget to use IPv4 only.
    
    Change-Id: I978b044d0e25f6316dd5aa758234e99cd90bcabb

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,126 +3,56 @@
 set -e
 
 # Download models
-BASE_URL="https://analytics.wikimedia.org/published/wmf-ml-models/mint/20250514081434"
+BASE_URL=${BASE_URL:-"https://people.wikimedia.org/~santhosh"}
 BASE_MODEL_DIR="models"
-mkdir -p "$BASE_MODEL_DIR"
-
-download_models() {
-	local url="$1"
-	local dest_path="$2"
-
-	if [ "${USE_S3CMD:-false}" = true ]; then
-		echo "Downloading using s3cmd: $url"
-		s3cmd get "$url" "$dest_path"
-	else
-		echo "Downloading using wget: $url"
-		wget --no-verbose --show-progress --progress=bar:force:noscroll "$url" -O "$dest_path"
-	fi
-}
-
-push_tgz() {
-	local tgz_path="$MODEL_TGZ_PATH"
-	local target_dir="$MODEL_DIR"
-
-	echo "Extracting $tgz_path into $target_dir"
-	mkdir -p "$target_dir"
-	tar xvzf "$tgz_path" -C "$target_dir" --strip-components=1
-	rm -f "$tgz_path"
-}
-
-download_zipped_models() {
-	local base_url="$1"
-	local model_zip="$2"
-	local dest_dir="$3"
-
-	local model_dir="$dest_dir/${model_zip%.zip}"
-	local zip_path="$dest_dir/$model_zip"
-
-	if [ -d "$model_dir" ]; then
-		echo "$model_zip already extracted, skipping."
-	else
-		download_models "$base_url/$model_zip" "$zip_path"
-		pushd "$dest_dir" > /dev/null
-		unzip -o "$model_zip"
-		rm -f "$model_zip"
-		popd > /dev/null
-	fi
-}
+mkdir -p $BASE_MODEL_DIR
 
 # The big generic model
-MODEL_NAME="nllb200-600M"
-MODEL_TGZ="${MODEL_NAME}.tgz"
-MODEL_DIR="${BASE_MODEL_DIR}/${MODEL_NAME}"
-MODEL_BASE_URL="${BASE_URL}/nllb"
-MODEL_URL="${MODEL_BASE_URL}/${MODEL_TGZ}"
-MODEL_TGZ_PATH="${BASE_MODEL_DIR}/${MODEL_TGZ}"
-
-download_models "$MODEL_URL" "$MODEL_TGZ_PATH"
-push_tgz
+MODEL_BASE_URL="${BASE_URL}/nllb/nllb200-600M"
+MODEL_DIR="${BASE_MODEL_DIR}/nllb200-600M"
+mkdir -p $MODEL_DIR
+if [ -d "${MODEL_DIR}" ] && [ "$(ls -A $MODEL_DIR)" ]; then
+	echo "Model already exists, skipping download."
+else
+	echo "Downloading $MODEL_BASE_URL/nllb200-600M.tgz"
+	wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}/nllb200-600M.tgz" -P $BASE_MODEL_DIR
+	pushd $BASE_MODEL_DIR
+	tar xvzf "nllb200-600M.tgz"
+	rm -rf "nllb200-600M.tgz"
+	popd
+fi
 
 # Wikipedia optimized model with limited languages
-MODEL_NAME="nllb-wikipedia"
-MODEL_TGZ="${MODEL_NAME}.tgz"
-MODEL_DIR="${BASE_MODEL_DIR}/${MODEL_NAME}"
-MODEL_BASE_URL="${BASE_URL}/nllb"
-MODEL_URL="${MODEL_BASE_URL}/${MODEL_TGZ}"
-MODEL_TGZ_PATH="${BASE_MODEL_DIR}/${MODEL_TGZ}"
-
-download_models "$MODEL_URL" "$MODEL_TGZ_PATH"
-push_tgz
-
-# MADLAD-400 NMT model
-MODEL_NAME="madlad400-3b-ct2"
-MODEL_TGZ="${MODEL_NAME}.tgz"
-MODEL_DIR="${BASE_MODEL_DIR}/${MODEL_NAME}"
-MODEL_BASE_URL="${BASE_URL}/madlad400"
-MODEL_URL="${MODEL_BASE_URL}/${MODEL_TGZ}"
-MODEL_TGZ_PATH="${BASE_MODEL_DIR}/${MODEL_TGZ}"
-
-download_models "$MODEL_URL" "$MODEL_TGZ_PATH"
-push_tgz
-
-# Indictrans2 models for English to Indic languages
-MODEL_NAME="indictrans-en-indic"
-MODEL_TGZ="${MODEL_NAME}.tgz"
-MODEL_DIR="${BASE_MODEL_DIR}/${MODEL_NAME}"
-MODEL_BASE_URL="${BASE_URL}/indictrans2"
-MODEL_URL="${MODEL_BASE_URL}/${MODEL_TGZ}"
-MODEL_TGZ_PATH="${BASE_MODEL_DIR}/${MODEL_TGZ}"
-
-download_models "$MODEL_URL" "$MODEL_TGZ_PATH"
-push_tgz
-
-# Indictrans2 models for Indic languages to English
-MODEL_NAME="indictrans-indic-en"
-MODEL_TGZ="${MODEL_NAME}.tgz"
-MODEL_DIR="${BASE_MODEL_DIR}/${MODEL_NAME}"
-MODEL_BASE_URL="${BASE_URL}/indictrans2"
-MODEL_URL="${MODEL_BASE_URL}/${MODEL_TGZ}"
-MODEL_TGZ_PATH="${BASE_MODEL_DIR}/${MODEL_TGZ}"
-
-download_models "$MODEL_URL" "$MODEL_TGZ_PATH"
-push_tgz
-
-# Indictrans2 models for translating between Indic languages
-MODEL_NAME="indictrans2-indic-indic"
-MODEL_TGZ="${MODEL_NAME}.tgz"
-MODEL_DIR="${BASE_MODEL_DIR}/${MODEL_NAME}"
-MODEL_BASE_URL="${BASE_URL}/indictrans2"
-MODEL_URL="${MODEL_BASE_URL}/${MODEL_TGZ}"
-MODEL_TGZ_PATH="${BASE_MODEL_DIR}/${MODEL_TGZ}"
-
-download_models "$MODEL_URL" "$MODEL_TGZ_PATH"
-push_tgz
+MODEL_BASE_URL="${BASE_URL}/nllb/nllb-wikipedia"
+MODEL_DIR="${BASE_MODEL_DIR}/nllb-wikipedia"
+for i in config.json model.bin sentencepiece.bpe.model shared_vocabulary.txt
+do
+	if [ -f "${MODEL_DIR}/${i}" ]; then
+		continue
+	else
+		echo "Downloading $MODEL_BASE_URL/${i}"
+		wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}"/${i} -P $MODEL_DIR
+	fi
+done
 
 # OpusMT optimized model with limited languages
-MODEL_BASE_URL="${BASE_URL}/opusmt"
+MODEL_BASE_URL=${BASE_URL}/opusmt
+MODEL_DIR=${BASE_MODEL_DIR}
 for i in opusmt-en-bcl.zip opusmt-en-bi.zip opusmt-en-chr.zip opusmt-en-guw.zip \
          opusmt-en-srn.zip opusmt-en-to.zip opusmt-en-ty.zip opusmt-en-ve.zip \
          opusmt-sv-fi.zip opusmt-fr-ty.zip opusmt-en-fr-br.zip
 do
-	MODEL_URL="${MODEL_BASE_URL}/${i}"
-	download_zipped_models "$MODEL_BASE_URL" "$i" "$BASE_MODEL_DIR"
+	if [ -d "${MODEL_DIR}/${i%.zip}" ]; then
+		continue
+	else
+		echo "Downloading $MODEL_BASE_URL/${i}"
+		wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}"/"${i}" -P $MODEL_DIR
+		# Extract the OpusMT optimized model
+		pushd $BASE_MODEL_DIR
+		unzip "${i}"
+		rm -rf "${i}"
+		popd
+	fi
 done
 
 # Softcatala NMT models
@@ -133,8 +63,71 @@ for i in softcatala-de-ca.zip softcatala-en-ca.zip softcatala-es-ca.zip softcata
          softcatala-it-ca.zip softcatala-ja-ca.zip softcatala-nl-ca.zip softcatala-oc-ca.zip softcatala-pt-ca.zip \
          softcatala-ca-it.zip softcatala-ca-ja.zip softcatala-ca-nl.zip softcatala-ca-oc.zip softcatala-ca-pt.zip
 do
-	MODEL_URL="${MODEL_BASE_URL}/${i}"
-	download_zipped_models "$MODEL_BASE_URL" "$i" "$BASE_MODEL_DIR"
+	if [ -d "${MODEL_DIR}/${i%.zip}" ]; then
+		continue
+	else
+		echo "Downloading $MODEL_BASE_URL/${i}"
+		wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}"/"${i}" -P $MODEL_DIR
+		# Extract the Softcatala optimized model
+		pushd $BASE_MODEL_DIR
+		unzip "${i}"
+		rm -rf "${i}"
+		popd
+	fi
+done
+
+# MADLAD-400 NMT model
+MODEL_BASE_URL="${BASE_URL}/madlad400-3b-ct2"
+MODEL_DIR="${BASE_MODEL_DIR}/madlad400-3b-ct2"
+mkdir -p $MODEL_DIR
+if [ -d "${MODEL_DIR}" ] && [ "$(ls -A $MODEL_DIR)" ]; then
+	echo "Model already exists, skipping download."
+else
+	echo "Downloading $MODEL_BASE_URL/madlad400-3b-ct2.tgz"
+	wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}/madlad400-3b-ct2.tgz" -P $BASE_MODEL_DIR
+	pushd $BASE_MODEL_DIR
+	tar xvzf "madlad400-3b-ct2.tgz"
+	rm -rf "madlad400-3b-ct2.tgz"
+	popd
+fi
+
+# Indictrans2 models for Indic languages to English
+MODEL_BASE_URL="${BASE_URL}/indictrans2/indictrans-indic-en"
+MODEL_DIR="${BASE_MODEL_DIR}/indictrans2-indic-en"
+for i in config.json model.bin model.SRC source_vocabulary.txt target_vocabulary.txt
+do
+	if [ -f "${MODEL_DIR}/${i}" ]; then
+		continue
+	else
+		echo "Downloading $MODEL_BASE_URL/${i}"
+		wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}"/${i} -P $MODEL_DIR
+	fi
+done
+
+# Indictrans2 models for English to Indic languages
+MODEL_BASE_URL="${BASE_URL}/indictrans2/indictrans-en-indic"
+MODEL_DIR="${BASE_MODEL_DIR}/indictrans2-en-indic"
+for i in config.json model.bin model.SRC source_vocabulary.txt target_vocabulary.txt
+do
+	if [ -f "${MODEL_DIR}/${i}" ]; then
+		continue
+	else
+		echo "Downloading $MODEL_BASE_URL/${i}"
+		wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}"/${i} -P $MODEL_DIR
+	fi
+done
+
+# Indictrans2 models for translating between Indic languages
+MODEL_BASE_URL="${BASE_URL}/indictrans2/indictrans2-indic-indic"
+MODEL_DIR="${BASE_MODEL_DIR}/indictrans2-indic-indic"
+for i in config.json model.bin model.SRC source_vocabulary.txt target_vocabulary.txt
+do
+	if [ -f "${MODEL_DIR}/${i}" ]; then
+		continue
+	else
+		echo "Downloading $MODEL_BASE_URL/${i}"
+		wget -N --inet4-only --no-verbose --show-progress --progress=bar:force:noscroll "${MODEL_BASE_URL}"/${i} -P $MODEL_DIR
+	fi
 done
 
 echo "Starting server..."
